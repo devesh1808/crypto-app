ddbce261d4fc7d080e70411c35ced94040f022aa
devesh1808 <devesh1808@gmail.com>
2024-01-03 23:21:32 +0530

    made a new file cryptoApi inside services folder to do the logic by fetching the crypto api

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,17 @@
+// const options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/coins',
+//     params: {
+//       referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//       timePeriod: '24h',
+//       'tiers[0]': '1',
+//       orderBy: 'marketCap',
+//       orderDirection: 'desc',
+//       limit: '50',
+//       offset: '0'
+//     },
+//     headers: {
+//       'X-RapidAPI-Key': 'f30b13464cmsh7749cee0c64078bp1dfa09jsnb7c7114f1c49',
+//       'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+//     }
+//   };
